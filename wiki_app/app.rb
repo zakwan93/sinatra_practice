cdd4f8b027e359fce42d22b9e7c6b80f1ba18458
zakwan93 <zakwan_bhaiyat@yahoo.com>
2018-12-02 23:18:40 -0500

    Practiced Sinatra GET,POST,PUT ad DELETE method and created a simple wiki page

--- wiki_app/app.rb
+++ wiki_app/app.rb
@@ -1,5 +1,6 @@
 require "sinatra"
-
+# URI use to remove white spacr in url
+require "uri"
 # Use to run app remotly on other server
 # set :bind, "0.0.0.0"
 
@@ -11,11 +12,23 @@ def page_content(title)
 # 	return nil
 end
 
-# by default root path when you run app
-get "/" do
-	erb :welcome
+def save_content(title,content)
+	File.open("pages/#{title}.txt", "w") do |file|
+		file.print(content)
+	end
 end
 
+def delete_content(title)
+	File.delete("pages/#{title}.txt")
+end
+# by default root path when you run app
+get "/" do
+	erb :welcome, layout: :page
+end
+
+get "/new" do
+	erb :new, layout: :page
+end
 
 # this get method will return the file name
 # get "/:title" do
@@ -26,5 +39,29 @@ end
 get "/:title" do
 	@title = params[:title]
 	@content = page_content(@title)
-	erb :show
+	erb :show, layout: :page
+end
+
+get "/:title/edit" do
+	@title = params[:title]
+	@content = page_content(@title)
+	erb :edit, layout: :page
+end
+
+put "/:title/edit" do
+	save_content(params["title"], params["content"])
+	redirect URI.escape("/#{params[:title]}")
+end
+
+post "/create" do
+	# this method is use to inspect the values we are passsing in parameteres with it's name.
+	# params.inspect
+	save_content(params["title"], params["content"])
+	# we are using URI to escape WHITE SPACE in between title from URL
+	redirect URI.escape("/#{params[:title]}")
+end
+ 
+delete "/:title" do
+	delete_content(params[:title])
+	redirect "/"
 end
